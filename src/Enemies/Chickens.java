593bd0f02e05c33eb27fecac79a156fbe9696233
FatmaAbdelmotagly <fatmaabdelmotagly195@gmail.com>
2025-12-10 22:08:25 +0200

    ..

--- src/Enemies/Chickens.java
+++ src/Enemies/Chickens.java
@@ -1,9 +1,8 @@
 package Enemies;
-
 public class Chickens {
     int x,y;
-    public Chickens(int x, int y){
-        this.x=x;
-        this.y=y;
+    public Chickens(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
