e978b87362f79bf8ebb0170cb6f047c8c3e5478a
Dina-Ebrahim <dinaaeebrahim@gmail.com>
2025-12-09 23:57:33 -0800

    Add.

--- src/Player/SpaceShip.java
+++ src/Player/SpaceShip.java
@@ -1,4 +1,93 @@
 package Player;
 
+import javax.media.opengl.GL;
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureIO;
+import java.io.File;
+import java.io.IOException;
+import texture.constants;
+
 public class SpaceShip {
+    private float x, y;
+    private int width, height;
+    private Texture texture;
+    private String imagePath;
+
+    public SpaceShip(String imagePath) {
+        this.width = constants.PLAYER_WIDTH;
+        this.height = constants.PLAYER_HEIGHT;
+        this.x = constants.WINDOW_WIDTH / 2 - width / 2;
+        this.y = 50;
+        this.imagePath = imagePath;
+    }
+
+    public void init(GL gl) {
+        try {
+            texture = TextureIO.newTexture(new File(imagePath), true);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(GL gl) {
+        if (texture == null) return;
+
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+        texture.enable();
+        texture.bind();
+
+
+        float drawX = 2.0f * x / constants.WINDOW_WIDTH - 1.0f;
+        float drawY = 2.0f * y / constants.WINDOW_HEIGHT - 1.0f;
+
+
+        float halfWidth = (float) width / constants.WINDOW_WIDTH;
+        float halfHeight = (float) height / constants.WINDOW_HEIGHT;
+
+        gl.glBegin(GL.GL_QUADS);
+        gl.glTexCoord2f(0, 1); gl.glVertex2f(drawX - halfWidth, drawY - halfHeight);
+        gl.glTexCoord2f(1, 1); gl.glVertex2f(drawX + halfWidth, drawY - halfHeight);
+        gl.glTexCoord2f(1, 0); gl.glVertex2f(drawX + halfWidth, drawY + halfHeight);
+        gl.glTexCoord2f(0, 0); gl.glVertex2f(drawX - halfWidth, drawY + halfHeight);
+        gl.glEnd();
+
+        texture.disable();
+        gl.glDisable(GL.GL_BLEND);
+    }
+
+    public void moveLeft() {
+        x -= constants.PLAYER_SPEED;
+        if(x < 0)
+            x = 0;
+    }
+    public void moveRight() {
+        x += constants.PLAYER_SPEED;
+        if(x + width > constants.WINDOW_WIDTH)
+            x = constants.WINDOW_WIDTH - width;
+    }
+    public void moveUp() {
+        y += constants.PLAYER_SPEED;
+        if(y + height > constants.WINDOW_HEIGHT)
+            y = constants.WINDOW_HEIGHT - height;
+    }
+    public void moveDown() {
+        y -= constants.PLAYER_SPEED;
+        if(y < 0)
+            y = 0;
+    }
+
+
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
 }
--- src/texture/constants.java
+++ src/texture/constants.java
@@ -4,11 +4,12 @@ public class constants {
     public static final int WINDOW_HEIGHT = 600;
 
     public static final float PLAYER_SPEED = 4.0f;
-    public static final int PLAYER_WIDTH = 80;
-    public static final int PLAYER_HEIGHT = 80;
+    public static final int PLAYER_WIDTH = 120;
+    public static final int PLAYER_HEIGHT = 120;
     public static final int FRAME_WIDTH = 1000;
     public static final int FRAME_HEIGHT = 800;
     public static final String BACKGROUND_PATH = "src/assets/back2.png";
+
 
 
 
